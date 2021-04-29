da0dc841c9fcd4a5c40071c8f1bdfef700b1a203
Adeyemi Olaoye <yhemi06@gmail.com>
2021-04-29 12:45:44 +0100

    Removed unused comments

--- src/redux/reducers/signupreducer.js
+++ src/redux/reducers/signupreducer.js
@@ -15,34 +15,6 @@ const userregstate = {
     }
 }
 
-// "id": 3,
-//     "email": "string",
-//     "firstName": "string",
-//     "lastName": "string",
-//     "createdDate": "2021-04-27T07:01:16.6194606-07:00",
-//     "userRole": "Customer",
-//     "phoneNumber": "string",
-//     "businessName": "string",
-//     "businessRegNumber": "string",
-//     "pin": "string",
-//     "country": null,
-//     "state": "string",
-//     "city": "string",
-//     "postalCode": "string",
-//     "address": "string",
-//     "gender": "string",
-//     "isKYCCompliancePassed": false,
-//     "isAMLCompliancePassed": false,
-//     "isScannedForAML": false,
-//     "dateOfBirth": "2021-04-26T17:17:21.011Z",
-//     "age": 0,
-//     "resetKey": "JCI-Reset-ITZTU",
-//     "referralDiscountCode": "JCI-DIS-52697786",
-//     "accountType": "Individual-Account",
-//     "receivers": [],
-//     "transactions": [],
-//     "customerReferredTransaction": null
-
 const getAuthState = () => {
     const auth = localStorage.getItem("auth");
     try {
@@ -61,7 +33,6 @@ const getAuthState = () => {
 //check state if exist before
 const existingAuth = getAuthState();
 
-//pass it here
 const signupreducer = (state = existingAuth, action) => {
 
     switch (action.type) {
@@ -73,18 +44,6 @@ const signupreducer = (state = existingAuth, action) => {
             };
             localStorage.setItem("auth", JSON.stringify(newAuthstate)); 
             return newAuthstate;
-        // case AuthActionType.LOGOUT_SUCCESS:
-        //     localStorage.removeItem("auth");
-        //     return authstate;
-        // case AuthActionType.LOGIN_SUCCESS:
-        //     const newAuthstatel = {
-        //         isLoggedIn: true,
-        //         isLoading: false,
-        //         userType: '', //TODO: this is hard coded for now, until the api returns apppropriate response
-        //         user: action.payload.userDetails,
-        //     };
-        //     localStorage.setItem("auth", JSON.stringify(newAuthstatel)); 
-        //     return newAuthstatel;
         default:
             return state;
     }
